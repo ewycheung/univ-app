0f93b9d7e566b182d51b364c0d555f440b32b049
ewycheung <cheung_eddie@rogers.com>
2021-04-12 20:23:20 -0400

    initial commit

---
+++ app/javascript/packs/application.js
@@ -0,0 +1,21 @@
+// This file is automatically compiled by Webpack, along with any other files
+// present in this directory. You're encouraged to place your actual application logic in
+// a relevant structure within app/javascript and only use these pack files to reference
+// that code so it'll be compiled.
+
+import Rails from "@rails/ujs"
+import Turbolinks from "turbolinks"
+import * as ActiveStorage from "@rails/activestorage"
+import "channels"
+import "jquery"
+import 'materialize-css/dist/js/materialize'
+
+Rails.start()
+Turbolinks.start()
+ActiveStorage.start()
+
+$(document).on('turbolinks:load', function() {
+  $(".sidenav").sidenav();  
+  $(".dropdown-trigger").dropdown();  
+  $("#fade-out-target").fadeOut(4000);
+});
